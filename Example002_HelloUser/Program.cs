f5d97ba2b2bab7019c38f5e57174bf4735a4bf38
Romanson <betinvetments@gmail.com>
2023-06-07 21:24:31 +0300

    добавил Program.cs

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,8 @@
+﻿//string username = Console.Readline();
+//Console.Writeline("Привет, ");
+//Console.Writeline(username);
+
+Console.Write("Vvedi imay, pridyrok");
+string username = Console.ReadLine();
+Console.Write("Privet, ");
+Console.Write(username);
