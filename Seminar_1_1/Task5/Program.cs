3466b2ed4dd6ecb5ef632d780554c52b62bc0019
Romanson <betinvetments@gmail.com>
2023-06-09 12:51:24 +0300

    Сделали task 5 и task 7

---
+++ Seminar_1_1/Task5/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача №5. Напишите программу, которая 
+// на вход принимает одно число (N), 
+// а на выходе показывает все целые числа 
+// в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+// 2 -> " -2, -1, 0, 1, 2"
+
+Console.WriteLine("Введите число N: ");
+int.TryParse(Console.ReadLine(), out int result);
+
+string str1 = "";
+for (int i = -result; i <= result; i++)
+{
+    if (i == -result)
+    {
+        str1 = i.ToString();
+    }
+    else
+    {
+        str1 += ", " + i.ToString();
+    }
+}
+Console.WriteLine($" {result} -> \" {str1} \"");
