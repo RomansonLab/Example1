f36315ddc4f6e8a0fcefbe5ffc9a5e5fe508d68f
Romanson <betinvetments@gmail.com>
2023-06-10 11:18:56 +0300

    add hometask 1.4

---
+++ Hometask_1/Task_1.4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число  ");
+int.TryParse(Console.ReadLine(), out int a);
+int i = 1;
+
+while (i <= a)
+{
+    int result = i % 2;
+    if (result == 0)
+    {
+        Console.Write(i + " ");
+    }
+    else
+    {
+        
+    }
+    i++;
+}
