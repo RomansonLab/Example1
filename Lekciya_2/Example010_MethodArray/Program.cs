9e17b785b1d8c67444c3fbd127c0d40590522cbf
Romanson <betinvetments@gmail.com>
2023-06-12 11:22:48 +0300

    add Lekciya 2 + all tasks

---
+++ Lekciya_2/Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1,2,32,43,43,333,23,32,4};
+
+int n = array.Length;
+int find = 43;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
---
+++ Lekciya_2/Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,55 @@
+﻿//  void - функция или метод, который ничего не возвращает
+//      в результате. Т.е. не надо например писать return в конце
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++; 
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 4;
+array[6] = 4;
+
+PrintArray(array);
+Console.WriteLine("_____");
+
+int pos = IndexOf(array, 444);
+Console.WriteLine(pos);
