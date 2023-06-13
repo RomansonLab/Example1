757575c85297ae536d96ee5382678a83ea3ec863
Romanson <betinvetments@gmail.com>
2023-06-13 13:49:11 +0300

    add all tasks from Hometask_2

---
+++ Hometask_2/Task001/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 10: Напишите программу, которая принимает на
+// вход трёхзначное число и на выходе показывает вторую
+// цифру этого числа
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+Console.WriteLine("Введите трехзначное число");
+int.TryParse(Console.ReadLine(), out int num1);
+int result;
+
+if (num1 < 100 | num1 > 999)
+{
+    Console.WriteLine("Тебе же сказали ввести ТРЕХзначное число, а ты что ввел?");
+}
+else
+{
+    result = num1 / 10;
+    result = result % 10;
+    Console.WriteLine($"{num1}, вторая цифра -> {result}");
+}
+
---
+++ Hometask_2/Task002/Program.cs
@@ -0,0 +1,28 @@
+﻿
+// Задача 13: Напишите программу, которая выводит
+// третью цифру заданного числа или сообщает, что третьей
+// цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int.TryParse(Console.ReadLine(), out int num1);
+int result;
+
+if (num1 < 100)
+{
+    Console.WriteLine("Третей цифры нет");
+}
+else
+{
+    while(num1 / 10 > 99)
+    {
+        num1 = num1 / 10;
+    }
+    result = num1 % 10;
+    Console.WriteLine(result);
+    
+}
